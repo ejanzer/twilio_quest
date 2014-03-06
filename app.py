1b44d912724b8422ed197fe5ee0e6da341f5a169
Hackbright Student <user@mini-004.(none)>
2014-03-05 16:16:30 -0800

    finished level 4

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 # Create a Flask web app
 app = Flask(__name__)
 
+recordings = []
+
 # Render the home page
 @app.route('/')
 def index():
@@ -53,6 +55,59 @@ def hello():
     response = twiml.Response()
     response.say('Hello there! You have successfully configured a web hook.')
     response.say('Good luck on your Twilio quest!', voice='woman')
+    return Response(str(response), mimetype='text/xml')
+
+@app.route('/handle_key1', methods=['GET', 'POST'])
+def handle_key1():
+    #Handle first key press from user.
+    digit_pressed = request.values.get('Digits', None)
+    if digit_pressed == "1":
+        response = twiml.Response()
+        response.say('Hello there! You are #1!')
+        return Response(str(response), mimetype='text/xml')
+    elif digit_pressed == "2"    :
+        response = twiml.Response()
+        response.say('Hello there! You are Slitherin!')
+        return Response(str(response), mimetype='text/xml')
+    elif digit_pressed == "3":
+        response = twiml.Response()
+        response.say("Leave a message!")
+        response.record(maxLength="30", action="/handle-recording")
+        return Response(str(response), mimetype='text/xml')
+    elif digit_pressed == "4":
+        response = twiml.Response()
+        if recordings == []:
+            response.say("There are no recorded messages.")
+        else: response.play(recordings[-1])
+        return Response(str(response), mimetype='text/xml')
+    else:
+        response = twiml.Response()
+        response.say('Goodbye.')
+        return Response(str(response), mimetype='text/xml')
+
+@app.route('/handle-recording', methods=['GET', 'POST'])
+def handle_recording():
+    recording_url = request.values.get("RecordingUrl", None)
+    recordings.append(recording_url)
+    response = twiml.Response()
+    response.say("Thanks for hissing. Listen to what you hissed.")
+    response.play(recording_url)
+    response.say("Goodbye.") 
+    return Response(str(response), mimetype='text/xml')
+
+
+@app.route('/incoming/call')
+def incoming_call():
+    response = twiml.Response()
+    with response.gather(numDigits=1, action="/handle_key1", method="POST") as g:
+        g.say('Press 1 for operator, 2 for a SNAKE!, 3 for leave a message, 4 to hear the last message.')
+
+    return Response(str(response), mimetype='text/xml')
+
+@app.route('/incoming/sms')
+def incoming_sms():
+    response = twiml.Response()
+    response.sms('What is black, white and red all over?')
     return Response(str(response), mimetype='text/xml')
 
 if __name__ == '__main__':
